6991324bf3880784bbb0ab5f32f22d060514f32d
Cash Williams <CashWilliams@gmail.com>
2015-01-14 22:33:01 -0600

    Add acquia host as known host and better rsync

--- acquia-deploy.sh
+++ acquia-deploy.sh
@@ -16,10 +16,13 @@ fi
 chmod 600 travis_id_rsa
 ssh-add travis_id_rsa
 
+# add acquia host to known ssh hosts
+ACQUIA_HOST=$(echo $ACQUIA_REPO | awk '{split($0, arr, "[@:]"); print arr[2]}')
+ssh-keyscan $ACQUIA_HOST >> ~/.ssh/known_hosts
+
 # clone acquia repo as /build_deploy
 git clone -b $TRAVIS_BRANCH $ACQUIA_REPO build_deploy
-rsync build/docroot build_deploy/docroot
-rsync build/hooks build_deploy/hooks
+rsync -a --delete build build_deploy
 
 cd build_deploy
 # git add -A should remove no longer used files
